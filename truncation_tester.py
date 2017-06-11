9535888058ae43be796fd185c875ce104aedfd99
Matthew Lancellotti <mareoraft@gmail.com>
2017-06-11 17:41:49 -0400

    Using python-Levenshtein library to compute word distances (lev.distance).  Added SublimeText things to .gitignore.

--- truncation_tester.py
+++ truncation_tester.py
@@ -1,4 +1,6 @@
 import pandas
+import Levenshtein as lev
+
 
 class TruncatorTester:
     def test_file(self, result_csv_filepath):
@@ -9,13 +11,16 @@ class TruncatorTester:
         truncation_collisions = self.summarize_truncation_collisions(df)
         raw_character_differences = self.count_raw_character_difference(df)
         total_character_saves = raw_character_differences["raw_character_difference"].sum()
+        lev_distances = self.count_lev_distance(df)
+        total_lev_dist = lev_distances["lev_distance"].sum()
         number_test_words = self.count_total_words(df)
         return {"total_collisions": truncation_collisions,
                 "total_character_saves": total_character_saves,
-                "total_words_evaluated": number_test_words}
+                "total_words_evaluated": number_test_words,
+                "total_lev_distance": total_lev_dist,}
 
-    def count_truncation_collisions(self, df): 
-        duplicate_counts = df.groupby(["Truncation"]).size()       
+    def count_truncation_collisions(self, df):
+        duplicate_counts = df.groupby(["Truncation"]).size()
         return duplicate_counts
 
     def summarize_truncation_collisions(self, df):
@@ -24,11 +29,15 @@ class TruncatorTester:
         adjusted_duplicate_counts = duplicate_counts-1
         total_collisions = adjusted_duplicate_counts.sum()
         return total_collisions
-    
+
     def count_raw_character_difference(self, df):
         def raw_character_difference(word1, word2):
-            return len(word1)-len(word2)        
+            return len(word1)-len(word2)
         df["raw_character_difference"] = list(map(raw_character_difference, df["Word"], df["Truncation"]))
+        return df
+
+    def count_lev_distance(self, df):
+        df["lev_distance"] = list(map(lev.distance, df["Word"], df["Truncation"]))
         return df
 
     def count_total_words(self,df):
