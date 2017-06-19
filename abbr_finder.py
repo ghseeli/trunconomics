47491082421a76f38d910e23bb88ff12cccfd7cb
Matthew Lancellotti <mareoraft@gmail.com>
2017-06-19 19:09:59 -0400

    use lev distance to weed out false abbreviations

--- abbr_finder.py
+++ abbr_finder.py
@@ -1,6 +1,7 @@
 """ These functions take in a list of words (from a corpus), and tries to identify abbreviations by looking at which words appear close to each other.
 """
 import copy
+import Levenshtein as lev
 
 def make_assoc_dic(word_list, association_radius):
 	""" Make an association dictionary """
@@ -53,13 +54,25 @@ def get_strong_associates(assoc_dic, word, distance, strong_threshold):
 			strong_associates.update(get_strong_associates(assoc_dic, strong_associate, distance - 1, strong_threshold))
 	return strong_associates
 
-def get_synonyms(assoc_dic, word, strong_threshold, weak_threshold):
+def get_potential_synonyms(assoc_dic, word, strong_threshold, weak_threshold):
 	strong_assoc2 = get_strong_associates(assoc_dic, word, 2, strong_threshold)
 	word_dic = assoc_dic[word]
 	neighbors = set(word_dic.keys())
 	non_weak_neighbors = {neighbor for neighbor in neighbors if word_dic[neighbor] > weak_threshold}
 	synonyms = strong_assoc2 - non_weak_neighbors
 	return synonyms
+
+def weed_out_synonyms(word, potential_synonyms):
+	""" Takes in a word and a list of 'supposed' synonyms, and deletes the ones whose Levenshtein distance is too high. """
+	real_synonyms = set()
+	for synonym in potential_synonyms:
+		max_distance = abs(len(word) - len(synonym))
+		abbr_len = min(len(word), len(synonym))
+		forgiveness = 1 + 1/7 * abbr_len
+		if lev.distance(word, synonym) <= max_distance + forgiveness:
+			# Then it's a synonym!
+			real_synonyms.add(synonym)
+	return real_synonyms
 
 # Below are three little tests, which should really be in a separate file:
 word_list = ['Conditional', 'and', 'unconditional', 'convergence', 'Operator', 'Theory', 'Advances', 'Basel', 'and', 'Applications', '94', 'Translated', 'by', 'Andrei', 'Iacob', 'from', 'the', 'and', 'Russian-language', 'Basel', 'Birkh', 'user', 'Verlag', 'pp', 'viii', '156', 'ISBN', '3-7643-5401-1', 'MR', '1442255', 'Weisstein', 'Eric', '2005', 'Riemann', 'Series', 'Theorem', 'Retrieved', 'May', '16', '2005']
@@ -75,6 +88,14 @@ print(strong_assoc)
 sentence = 'bla bla the characteristic of a field is p  The char of a field is p'
 word_list = sentence.split()
 assoc_dic = make_assoc_dic(word_list, 2)
-synonyms = get_synonyms(assoc_dic, "characteristic", 9.0, 1.0)
+synonyms = get_potential_synonyms(assoc_dic, "characteristic", 9.0, 1.0)
 print(synonyms)
+print()
 
+word = "characteristic"
+sentence = 'bla bla the characteristic of a field is p  The char of a field is p'
+word_list = sentence.split()
+assoc_dic = make_assoc_dic(word_list, 2)
+potential_synonyms = get_potential_synonyms(assoc_dic, word, 9.0, 1.0)
+synonyms = weed_out_synonyms(word, potential_synonyms)
+print(synonyms)
