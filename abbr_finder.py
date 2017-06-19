aaede958bf21ed8c19267a9087702f6379564e38
Matthew Lancellotti <mareoraft@gmail.com>
2017-06-19 18:45:55 -0400

    abbr_finder now finds potential synonyms

--- abbr_finder.py
+++ abbr_finder.py
@@ -34,6 +34,47 @@ def add_block_to_dic(word_dic, block):
 		association = association/2.0
 	return word_dic
 
+def is_non_word(word, dictionary):
+	return word in dictionary.keys()
+
+def get_strong_associates(assoc_dic, word, distance, strong_threshold):
+	""" Returns a SET of strong associates equal to distance or CLOSER than given distance. """
+	if distance < 0:
+		raise ValueError
+	if distance == 0:
+		strong_associates = {word}
+	else:
+		word_dic = assoc_dic[word]
+		print(word_dic)
+		strong_associates = {associate for associate in word_dic.keys() if word_dic[associate] >= strong_threshold}
+		print(strong_associates)
+		strong_associates_copy = copy.deepcopy(strong_associates)
+		for strong_associate in strong_associates_copy:
+			strong_associates.update(get_strong_associates(assoc_dic, strong_associate, distance - 1, strong_threshold))
+	return strong_associates
+
+def get_synonyms(assoc_dic, word, strong_threshold, weak_threshold):
+	strong_assoc2 = get_strong_associates(assoc_dic, word, 2, strong_threshold)
+	word_dic = assoc_dic[word]
+	neighbors = set(word_dic.keys())
+	non_weak_neighbors = {neighbor for neighbor in neighbors if word_dic[neighbor] > weak_threshold}
+	synonyms = strong_assoc2 - non_weak_neighbors
+	return synonyms
+
+# Below are three little tests, which should really be in a separate file:
 word_list = ['Conditional', 'and', 'unconditional', 'convergence', 'Operator', 'Theory', 'Advances', 'Basel', 'and', 'Applications', '94', 'Translated', 'by', 'Andrei', 'Iacob', 'from', 'the', 'and', 'Russian-language', 'Basel', 'Birkh', 'user', 'Verlag', 'pp', 'viii', '156', 'ISBN', '3-7643-5401-1', 'MR', '1442255', 'Weisstein', 'Eric', '2005', 'Riemann', 'Series', 'Theorem', 'Retrieved', 'May', '16', '2005']
 assoc_dic = make_assoc_dic(word_list, 2)
 print(assoc_dic['and'])
+
+sentence = 'bla bla the characteristic of a field is p  The char of a field is p'
+word_list = sentence.split()
+assoc_dic = make_assoc_dic(word_list, 2)
+strong_assoc = get_strong_associates(assoc_dic, "characteristic", 2, 9.0)
+print(strong_assoc)
+
+sentence = 'bla bla the characteristic of a field is p  The char of a field is p'
+word_list = sentence.split()
+assoc_dic = make_assoc_dic(word_list, 2)
+synonyms = get_synonyms(assoc_dic, "characteristic", 9.0, 1.0)
+print(synonyms)
+
