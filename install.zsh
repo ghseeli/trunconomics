b2f35da75a3429b402a89578bb17a2a58d9aa389
Matthew Lancellotti <mareoraft@gmail.com>
2017-06-29 00:57:27 -0400

    install script!!!!!!!!

---
+++ install.zsh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+
+# Install Levenshtein (but import as 'Levenshtein' in python)
+pip3 install python-Levenshtein
+
+# Install pyparsing
+## Install bs4, even if just to get a pointer to BeautifulSoup that you already have
+pip3 install bs4
+pip3 install pyparsing
+
+# Install pygame
+## Install SDL, but only if you don't have the file SDL.h.
+brew install sdl
+pip3 install pygame
+
+# Install pandas
+pip3 install pandas
+
+# Install pytest (but use command 'py.test' in terminal)
+pip3 install pytest
+
+# Install mediawikiapi (MediaWikiAPI is compatible only with Python 3.3+)
+pip3 install mediawikiapi
